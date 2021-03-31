0b4cdb622a6489a155fe229c0913c5d4bb0b2e58
taiduc2208 <63004706+taiduc2208@users.noreply.github.com>
2021-03-31 23:35:26 +0700

    Abu up 2.2, 2.4

---
+++ 2.4.basicform.php
@@ -0,0 +1,15 @@
+<html>
+
+    <head><title>A Simple Form</title></head>
+    <body>
+        <form action="http://fit.hust.edu.vn/~trangntt/course/wp" 
+        method="POST">
+            Click submit to start our initial PHP program.
+            <br>
+            <input type="text" name="Name">
+            <br>
+            <input type="submit" value="Click to Submit">
+            <input type="reset" value="Erase and Restart">
+        </form>
+    </body>
+</html>
